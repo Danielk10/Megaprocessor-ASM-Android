5372e5998d7330ddd37fe7083ad0f77fec03eafc
Danielk10 <danielpdiamon@gmail.com>
2026-02-10 18:48:27 -0400

    Nuevo

---
+++ app/src/main/cpp/utils.cpp
@@ -0,0 +1,30 @@
+#include "utils.h"
+#include <sstream>
+#include <algorithm>
+
+std::string trim(const std::string& str) {
+    size_t first = str.find_first_not_of(" \t\r\n");
+    if (std::string::npos == first) {
+        return str;
+    }
+    size_t last = str.find_last_not_of(" \t\r\n");
+    return str.substr(first, (last - first + 1));
+}
+
+std::string toUpper(const std::string& str) {
+    std::string result = str;
+    std::transform(result.begin(), result.end(), result.begin(), ::toupper);
+    return result;
+}
+
+std::vector<std::string> split(const std::string& str, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(str);
+    while (std::getline(tokenStream, token, delimiter)) {
+        if (!token.empty()) {
+            tokens.push_back(token);
+        }
+    }
+    return tokens;
+}
--- app/src/main/cpp/utils.h
+++ app/src/main/cpp/utils.h
@@ -2,35 +2,15 @@
 #define UTILS_H
 
 #include <string>
-#include <algorithm>
 #include <vector>
-#include <sstream>
+#include <android/log.h>
 
-// Función para eliminar espacios en blanco al inicio y final
-static inline std::string trim(const std::string& str) {
-    size_t first = str.find_first_not_of(" \t\r\n");
-    if (std::string::npos == first) {
-        return str;
-    }
-    size_t last = str.find_last_not_of(" \t\r\n");
-    return str.substr(first, (last - first + 1));
-}
+#define LOG_TAG "MegaprocessorASM"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-// Función para convertir a mayúsculas
-static inline std::string toUpper(std::string str) {
-    std::transform(str.begin(), str.end(), str.begin(), ::toupper);
-    return str;
-}
-
-// Función para dividir string
-static inline std::vector<std::string> split(const std::string& str, char delimiter) {
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream tokenStream(str);
-    while (std::getline(tokenStream, token, delimiter)) {
-        tokens.push_back(token);
-    }
-    return tokens;
-}
+std::string trim(const std::string& str);
+std::string toUpper(const std::string& str);
+std::vector<std::string> split(const std::string& str, char delimiter);
 
 #endif // UTILS_H
